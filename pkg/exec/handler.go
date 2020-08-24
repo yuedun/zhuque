733153ae03088505e343f75b0f89d9e2da9e4a62
hale.huo <hale.huo@zhangmen.com>
2020-08-24 15:04:35 +0800

    发布记录

--- pkg/exec/handler.go
+++ pkg/exec/handler.go
@@ -8,11 +8,13 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yuedun/zhuque/db"
+	"github.com/yuedun/zhuque/pkg/task"
 
 	"os/exec"
 )
 
-//发送命令到服务器
+// Send 发送命令到服务器
 func Send(c *gin.Context) {
 	defer func() {
 		if err := recover(); err != nil {
@@ -49,7 +51,7 @@ func Send(c *gin.Context) {
 	})
 }
 
-//发送命令到服务器
+// Server 快捷发布，只需要选择项目即可发布
 func Server(c *gin.Context) {
 	defer func() {
 		if err := recover(); err != nil {
@@ -58,20 +60,38 @@ func Server(c *gin.Context) {
 			})
 		}
 	}()
-	userCmd, ok := c.GetPostForm("cmd")
-	if !ok || userCmd == "" {
+	cmdParam, ok := c.GetPostForm("cmd")
+	if !ok || cmdParam == "" {
 		panic(errors.New("命令无效！"))
 	}
-	userCmd = fmt.Sprintf("pm2 deploy projects/%s/ecosystem.config.js production --force", userCmd)
+	userID, ok := c.GetPostForm("userID")
+	if !ok || userID == "" {
+		panic(errors.New("用户ID无效！"))
+	}
+	username, ok := c.GetPostForm("username")
+	if !ok || username == "" {
+		panic(errors.New("用户名无效！"))
+	}
+	userCmd := fmt.Sprintf("pm2 deploy projects/%s/ecosystem.config.js production --force", cmdParam)
 	log.Println("用户输入命令：", userCmd)
 	var cmdOut []byte
 	var err error
 	var cmd *exec.Cmd
 	// 执行单个shell命令时, 直接运行即可
 	cmd = exec.Command("bash", "-c", userCmd)
+	taskServer := task.NewService(db.SQLLite)
+	task := task.Task{
+		TaskName: "",
+		Project:  cmdParam,
+		UserID:   userID,
+		Username: username,
+	}
+
 	if cmdOut, err = cmd.CombinedOutput(); err != nil {
 		log.Println("输出错误：", err)
 		log.Println("输出错误2：", string(cmdOut))
+		task.ReleaseResult = 0       //失败
+		taskServer.CreateTask(&task) //保存失败发布记录
 		c.JSON(200, gin.H{
 			"message": err,
 			"data":    strings.ReplaceAll(string(cmdOut), "\n", "<br>"),
@@ -80,7 +100,8 @@ func Server(c *gin.Context) {
 	}
 	// 默认输出有一个换行
 	log.Println(string(cmdOut))
-
+	task.ReleaseResult = 1       //失败
+	taskServer.CreateTask(&task) //保存成功发布记录
 	c.JSON(200, gin.H{
 		"message": "ok",
 		"data":    strings.ReplaceAll(string(cmdOut), "\n", "<br>"),
--- pkg/task/task-model.go
+++ pkg/task/task-model.go
@@ -3,15 +3,17 @@ package task
 import "time"
 
 type Task struct {
-	ID        int       `json:"id"`
-	TaskName  string    `json:"taskName"`
-	Project   string    `json:"project"`
-	Username  string    `json:"username"`
-	CommitID  string    `json:"commitID"`
-	Env       string    `json:"env"`
-	Status    string    `json:"status"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID            int       `json:"id"`
+	TaskName      string    `json:"taskName"`
+	Project       string    `json:"project"`
+	UserID        string    `json:"userID"`
+	Username      string    `json:"username"`
+	CommitID      string    `json:"commitID"`
+	Env           string    `json:"env"`
+	Status        string    `json:"status"`        //数据状态：1有效，0无效
+	ReleaseResult int       `json:"releaseResult"` // 发布结果：1成功，0失败
+	CreatedAt     time.Time `json:"createdAt"`
+	UpdatedAt     time.Time `json:"updatedAt"`
 }
 
 // 设置User的表名为`user`
