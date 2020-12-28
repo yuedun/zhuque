c68ab9a8e911f6da2487556ee68d5d5ac6981403
hale.huo <hale.huo@zhangmen.com>
2020-12-28 17:07:35 +0800

    单任务发布只重启

--- pkg/exec/handler.go
+++ pkg/exec/handler.go
@@ -85,7 +85,7 @@ func Server(c *gin.Context) {
 	userCmd := fmt.Sprintf("pm2 deploy projects/%s/ecosystem.config.js production --force", cmdParam)
 	if restart == "on" {
 		// 由于pm2的项目名和管理的项目名不能完全保持一致，所以如果一个pm2下跑多个服务都只能重启，但是reload可以实现不停服重启
-		userCmd = fmt.Sprintf("pm2 deploy projects/%s/ecosystem.config.js production exec 'git pull && pm2 reload all' --force", cmdParam)
+		userCmd = fmt.Sprintf("pm2 deploy projects/%s/ecosystem.config.js production exec 'git pull && pm2 reload ecosystem.config.js' --force", cmdParam)
 	}
 	log.Println("用户输入命令：", userCmd)
 
