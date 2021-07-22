0e62f79bf4815eb233126f65d40859db7f7397c0
yuedun <light_v@163.com>
2021-07-22 17:38:31 +0800

    用户项目创建时间

--- pkg/user/user-project-model.go
+++ pkg/user/user-project-model.go
@@ -1,10 +1,13 @@
 package user
 
+import "time"
+
 type UserProject struct {
-	ID         int `json:"id" gorm:"AUTO_INCREMENT;primary_key"`
-	UserID     int `json:"userID"`
-	ProjectID  int `json:"projectID"`
-	CreateUser int `json:"createUser"`
+	ID         int       `json:"id" gorm:"AUTO_INCREMENT;primary_key"`
+	UserID     int       `json:"userID"`
+	ProjectID  int       `json:"projectID"`
+	CreateUser int       `json:"createUser"`
+	CreatedAt  time.Time `json:"createdAt"`
 }
 
 // 设置UserProject的表名为`user_project`
@@ -13,9 +16,10 @@ func (UserProject) TableName() string {
 }
 
 type UserProjectVO struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`       //项目名
-	Namespace  string `json:"namespace"`  //空间
-	Username   string `json:"username"`   //关联用户
-	CreateUser string `json:"createUser"` //创建者
+	ID          int       `json:"id"`
+	ProjectName string    `json:"projectName"` //项目名
+	Namespace   string    `json:"namespace"`   //空间
+	Username    string    `json:"username"`    //关联用户
+	CreateUser  string    `json:"createUser"`  //创建者
+	CreatedAt   time.Time `json:"createdAt"`
 }
